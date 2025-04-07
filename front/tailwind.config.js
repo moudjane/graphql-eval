c04ac35cb9764998796dac95e3e7691d526d109d
moudjane <mathis.oudjane@gmail.com>
2025-04-07 14:54:38 +0200

    update init tailwind

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,10 +1,17 @@
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'hn-orange': '#ff6600',
+      },
+      fontFamily: {
+        'mono': ['Courier New', 'monospace'],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }

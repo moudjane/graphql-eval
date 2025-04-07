1e7df59d66f201f011dcc7565ba0e3ab8aee6784
moudjane <mathis.oudjane@gmail.com>
2025-04-07 15:25:59 +0200

    update App.vue and add HomeView and update router

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="p-8">
+    <div class="mx-auto max-w-3xl">
+      <h1 class="text-4xl font-bold mb-4">Bienvenue</h1>
+      <p class="text-gray-600">
+        sur notre site de news tech
+      </p>
+    </div>
   </main>
 </template>

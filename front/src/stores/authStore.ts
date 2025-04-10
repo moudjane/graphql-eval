012ea2842a768fae32f994b659df4b2fe880f742
moudjane <mathis.oudjane@gmail.com>
2025-04-10 14:41:25 +0200

    add localstorage for users

--- front/src/stores/authStore.ts
+++ front/src/stores/authStore.ts
@@ -1,26 +1,28 @@
-import { defineStore } from 'pinia';
-import type { User } from '@/gql/graphql';
+import { defineStore } from 'pinia'
+import type { User } from '@/gql/graphql'
 
 interface AuthState {
-  token: string | null;
-  user: User | null;
+  token: string | null
+  user: User | null
 }
 
 export const useAuthStore = defineStore('auth', {
   state: (): AuthState => ({
     token: localStorage.getItem('authToken'),
-    user: null,
+    user: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')!) : null
   }),
   actions: {
     setAuth(authPayload: { token: string; user: User }): void {
-      this.token = authPayload.token;
-      this.user = authPayload.user;
-      localStorage.setItem('authToken', authPayload.token);
+      this.token = authPayload.token
+      this.user = authPayload.user
+      localStorage.setItem('authToken', authPayload.token)
+      localStorage.setItem('user', JSON.stringify(authPayload.user))
     },
     clearAuth(): void {
-      this.token = null;
-      this.user = null;
-      localStorage.removeItem('authToken');
-    },
-  },
-});
+      this.token = null
+      this.user = null
+      localStorage.removeItem('authToken')
+      localStorage.removeItem('user')
+    }
+  }
+})
